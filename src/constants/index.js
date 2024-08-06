ecf8f6cfe78fe938bc1a6fe6ec9e542b60691a71
shaikhmubin02 <shaikhmubin572@gmail.com>
2024-08-07 02:06:37 +0530

    init

---
+++ src/constants/index.js
@@ -0,0 +1,221 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Fullstack Developer",
+    icon: web,
+  },
+  {
+    title: "Machine Learning & AI",
+    icon: mobile,
+  },
+  {
+    title: "Quantitative Finance",
+    icon: backend,
+  },
+  {
+    title: "Competitive Programming",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Software Developer",
+    company_name: "Backdrop Labs",
+    icon: meta,
+    iconBg: "#383E56",
+    date: "March 2024 - May 2024",
+    points: [
+      " Worked on an experimental AI assistant using the GPT-4 Vision API to enable real-time interactions through video streaming and voice communication,leveraging Next.js and Vercel AI",
+      "Developed a real-time video analysis system, utilizing Whisper API for speech transcription and generating verbal responses, addressing technical constraints in AI-driven interfaces.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Mubin excels in AI with his innovative problem-solving skills and deep understanding of the field. His hackathon victory is a testament to his expertise.",
+    name: "Joe",
+    designation: "SWD",
+    company: "Backdrop Labs",
+  },
+  {
+    testimonial:
+      "Mubin’s machine learning skills are impressive, especially in high-frequency trading. His practical approach and strong analysis make him stand out.",
+    name: "Shivam",
+    designation: "Student",
+    company: "PDPU",
+  },
+  {
+    testimonial:
+      "Mubin is a top-notch ful- developer. His work with Next.js, TypeScript, and API integrations shows his skill and teamwork.",
+    name: "Sahil",
+    designation: "Student",
+    company: "GTU",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/shaikhmubin02",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/shaikhmubin02",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/shaikhmubin02",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };

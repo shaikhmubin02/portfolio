aed519b5e91ce55957536e1e253ba9ea9a7a092c
shaikhmubin02 <shaikhmubin572@gmail.com>
2024-08-10 09:43:05 +0530

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/logo.svg" sizes="any" />
+        <link rel="icon" href="/logo.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
